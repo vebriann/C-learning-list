64f4ebbad96a6489213271fd719e6c28f59e09d3
Vebrian Developer <vebrianns01@gmail.com>
2022-10-12 08:09:49 +0700

    tugas sekolah

---
+++ PercabangaC/PercabangaC/Program.cs
@@ -0,0 +1,91 @@
+﻿// 
+
+using System;
+
+class PercabanganC
+{
+    static void Main(String[] args)
+
+    {
+        string pilih;
+        int angka1;
+        int angka2;
+        int hasil;
+
+        Console.WriteLine(" Pilih aritmatika operation ");
+        Console.WriteLine(" 1. Penambahan ");
+        Console.WriteLine(" 2. Pengurangan ");
+        Console.WriteLine(" 3. Perkalian ");
+        Console.WriteLine(" 4. Pembagian ");
+        Console.WriteLine("");
+        Console.Write(" pilih: ");
+        pilih = Console.ReadLine();
+        if (pilih == "1")
+        {
+            Console.WriteLine("");
+            Console.WriteLine(" Masukan Nilai Pertama");
+            Console.Write(" Nilai: ");
+            angka1 = int.Parse(Console.ReadLine());
+            Console.WriteLine(" Masukan Nilai kedua");
+            Console.Write(" Nilai: ");
+            angka2 = int.Parse(Console.ReadLine());
+
+            hasil = angka1 + angka2;
+
+            Console.WriteLine("");
+            Console.WriteLine($" Hasil Dari Nilai {angka1} dan {angka2} adalah {hasil}");
+
+        }
+        else if (pilih == "2") 
+        {
+            Console.WriteLine("");
+            Console.WriteLine(" Masukan Nilai Pertama");
+            Console.Write(" Nilai: ");
+            angka1 = int.Parse(Console.ReadLine());
+            Console.WriteLine(" Masukan Nilai kedua");
+            Console.Write(" Nilai: ");
+            angka2 = int.Parse(Console.ReadLine());
+
+            hasil = angka1 - angka2;
+
+            Console.WriteLine("");
+            Console.WriteLine($" Hasil Dari Nilai {angka1} dan {angka2} adalah {hasil}");
+        }
+        else if (pilih == "3")
+        {
+            Console.WriteLine("");
+            Console.WriteLine(" Masukan Nilai Pertama");
+            Console.Write(" Nilai: ");
+            angka1 = int.Parse(Console.ReadLine());
+            Console.WriteLine(" Masukan Nilai kedua");
+            Console.Write(" Nilai: ");
+            angka2 = int.Parse(Console.ReadLine());
+
+            hasil = angka1 * angka2;
+
+            Console.WriteLine("");
+            Console.WriteLine($" Hasil Dari Nilai {angka1} dan {angka2} adalah {hasil}");
+        }
+        else if (pilih == "4")
+        {
+            Console.WriteLine("");
+            Console.WriteLine(" Masukan Nilai Pertama");
+            Console.Write(" Nilai: ");
+            angka1 = int.Parse(Console.ReadLine());
+            Console.WriteLine(" Masukan Nilai kedua");
+            Console.Write(" Nilai: ");
+            angka2 = int.Parse(Console.ReadLine());
+
+            hasil = angka1 / angka2;
+
+            Console.WriteLine("");
+            Console.WriteLine($" Hasil Dari Nilai {angka1} dan {angka2} adalah {hasil}");
+
+        }
+
+        else
+        {
+            Console.WriteLine(" maaf pilihan yang anda pilih tidak ada!");
+        }
+    }
+}
