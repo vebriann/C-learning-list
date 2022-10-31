42b806ad62ada56a68bfcdbeebdc25a6c56ebed7
Vebrian Developer <vebrianns01@gmail.com>
2022-10-31 09:28:01 +0700

    bermain switch case

---
+++ bermain switch case C#/NamaBulanString/NamaBulanString/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class NamaBulanString
+{
+    static void Main()
+    {
+        string s, namaBulan;
+        int noBulan= 0;
+        Console.Clear();
+        Console.Write("Masukan Nama Bulan: ");
+        s = Console.ReadLine();
+        namaBulan = s.ToLower();
+
+        switch (namaBulan)
+        {
+            case "januhari": noBulan = 1; break;
+            case "februari": noBulan = 2; break;
+            case "maret": noBulan = 3; break;
+            case "april": noBulan = 4; break;
+            case "mei": noBulan = 5; break;
+            case "juni": noBulan = 6; break;
+            case "juli": noBulan = 7; break;
+            case "agustus": noBulan = 8; break;
+            case "september": noBulan = 9; break;
+            case "oktober": noBulan = 10; break;
+            case "november": noBulan = 11; break;
+            case "desember": noBulan = 12; break;
+            default:
+                {
+                    Console.WriteLine($"maaf tidak ada bulan {s} di kalender"); Environment.Exit(1); break;
+                }
+        }
+        Console.WriteLine($"{s} adalah bulan ke-{noBulan}");
+
+    }
+}
---
+++ bermain switch case C#/NamaHariString/NamaHariString/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class NamaHariString
+{
+    static void Main()
+    {
+        string s, namahari;
+        int nohari = 0;
+        Console.Clear();
+        Console.Write("Masukan Nama Hari: ");
+        s = Console.ReadLine();
+        namahari = s.ToLower();
+
+        switch(namahari)
+        {
+            case "senin": nohari = 1; break;
+            case "selasa": nohari = 2; break;
+            case "rabu": nohari = 3; break;
+            case "kamis": nohari = 4; break;
+            case "jumat": nohari = 5; break;
+            case "sabtu": nohari = 6; break;
+            case "ahad": nohari = 7; break;
+            default:
+                {
+                    Console.WriteLine($"{s} Bukan nama hari"); Environment.Exit(1); break;
+                }
+        }
+        Console.WriteLine($"{s} adalah hari ke-{nohari}"); 
+
+    }
+}
