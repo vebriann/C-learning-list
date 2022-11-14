60782ebb6f38c5014694e84df09451b8905f667e
Vebrian Developer <vebrianns01@gmail.com>
2022-11-14 08:26:13 +0700

    update

---
+++ PerulanganFor/PerulanganFor/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PerulanganFor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ PerulanganForeach/PerulanganForeach/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Perulanganforeach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nama = new string[5] { "Adi",
+"Budi", "Caca", "Deni", "Lala" };
+            Console.WriteLine("\n Nama yang anda masukan adalah: ");
+        for (int a = 0; a < 5; a++)
+            {
+                Console.WriteLine(a + " " + nama[a] + " ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
